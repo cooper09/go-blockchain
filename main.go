722e5cafa66ec621303d68a86f8f164591ef00e4
cooper09 <cooperatkmba@gmail.com>
2021-03-30 14:08:46 +0000

    first proof of work

--- main.go
+++ main.go
@@ -4,43 +4,8 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"github.com/cooper09/go-blockchain/blockchain"
 )
-
-type BlockChain struct {
-	blocks []*Block
-}//end BlockChain
-
-type Block struct {
-	Hash		[]byte
-	Data		[]byte
-	PrevHash 	[]byte
-}//end Block
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}//end DeriveHash
-
-func CreateBlock (data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash }
-	block.DeriveHash()
-	return block
-}//end CreateBlock
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks) - 1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new )
-}//end AddBlock
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}//end Genesis
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}//end InitBlockchain
 
 func main() {
 	chain := InitBlockChain()
@@ -54,5 +19,9 @@ func main() {
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
 
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s/n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
 	}//end for
-}//end main
+}//end mainrun main
